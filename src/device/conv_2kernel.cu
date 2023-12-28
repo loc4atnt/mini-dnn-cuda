290cd4fe6b4e4e9aabb20756529e3449c618bde6
loc4atnt <dorakid153@gmail.com>
2023-12-28 23:48:34 +0700

    up

--- src/device/conv_2kernel.cu
+++ src/device/conv_2kernel.cu
@@ -2,7 +2,7 @@
 // input size: (height_in * width_in * channel_in)
 // data size: (hw_out * hw_kernel * channel_in)
 
-__global__ void im2col (float* input, float* data, int height_in, int width_in, int channel_in, int height_kernel, int width_kernel, 
+__global__ void im2col(float* input, float* data, int height_in, int width_in, int channel_in, int height_kernel, int width_kernel, 
 			int height_out, int width_out, int channel_out, int stride)
 {	
 	int i = blockIdx.y * blockDim.y + threadIdx.y;   // row: 0 - hw_out
@@ -45,7 +45,7 @@ __global__ void im2col (float* input, float* data, int height_in, int width_in, 
 // output size (m, k) - (hw_out, channel_out)
 // bias size (k) - (channel_out)
 
-__global__ void convolution (float* data, float* weight, float* output, float* bias, int m, int n, int k)
+__global__ void convolution(float* data, float* weight, float* output, float* bias, int m, int n, int k)
 {
 	int i = blockIdx.y * blockDim.y + threadIdx.y;
     	int j = blockIdx.x * blockDim.x + threadIdx.x;
@@ -61,7 +61,7 @@ __global__ void convolution (float* data, float* weight, float* output, float* b
 	}
 }
 
-__global__ void convolution_kernel2 (float* data, float* weight, float* output, float* bias, int m, int n, int k)
+__global__ void convolution_kernel2(float* data, float* weight, float* output, float* bias, int m, int n, int k)
 {
 	__shared__ float s_data[TILE_WIDTH][TILE_WIDTH];    //BLOCK HEIGHT, BLOCK WIDTH
 	__shared__ float s_weight[TILE_WIDTH][TILE_WIDTH];  //BLOCK HEIGHT, BLOCK WIDTH
@@ -107,59 +107,59 @@ __global__ void convolution_kernel2 (float* data, float* weight, float* output, 
 		// output[i * k + j] = s;
 	}
 }
-void Conv::forward(const Matrix& bottom, bool useDevice = false) 
-{
-	int n_sample = bottom.cols();
-	top.resize(height_out * width_out * channel_out, n_sample);
-	if (useDevice == false)
-	{
-		data_cols.resize(n_sample);
-		for (int i = 0; i < n_sample; i ++) 
-		{
-			// im2col
-			Matrix data_col;
-			im2col(bottom.col(i), data_col);
-			data_cols[i] = data_col;
+// void Conv::forward(const Matrix& bottom, bool useDevice = false) 
+// {
+// 	int n_sample = bottom.cols();
+// 	top.resize(height_out * width_out * channel_out, n_sample);
+// 	if (useDevice == false)
+// 	{
+// 		data_cols.resize(n_sample);
+// 		for (int i = 0; i < n_sample; i ++) 
+// 		{
+// 			// im2col
+// 			Matrix data_col;
+// 			im2col(bottom.col(i), data_col);
+// 			data_cols[i] = data_col;
 
-			// conv by product
-			Matrix result = data_col * weight;  // result: (hw_out, channel_out)
-			result.rowwise() += bias.transpose();
-			//Matrix result = matrixMul(data_col, weight);
-			//matrixRowwiseAddVec(result, bias);
+// 			// conv by product
+// 			Matrix result = data_col * weight;  // result: (hw_out, channel_out)
+// 			result.rowwise() += bias.transpose();
+// 			//Matrix result = matrixMul(data_col, weight);
+// 			//matrixRowwiseAddVec(result, bias);
 
-			top.col(i) = Eigen::Map<Vector>(result.data(), result.size());
-		}
-	}
-	else
-	{
-		for (int n = 0; n < n_sample; n++) 
-		{
-			Vector in = bottom.col(n);
-			//TODO: Copy data from in to d_input
+// 			top.col(i) = Eigen::Map<Vector>(result.data(), result.size());
+// 		}
+// 	}
+// 	else
+// 	{
+// 		for (int n = 0; n < n_sample; n++) 
+// 		{
+// 			Vector in = bottom.col(n);
+// 			//TODO: Copy data from in to d_input
 			
-			//TODO: Copy data from weight to d_weight
+// 			//TODO: Copy data from weight to d_weight
 			
-			//Allocate memories
-			float* d_data, * d_output;
-			size_t n_data = height_out * width_out * height_kernel * width_kernel * channel_in * sizeof(float);
-			size_t n_output = height_out * width_out * channel_out * sizeof(float);
-			CHECK(cudaMalloc(&d_data, n_data));
-			CHECK(cudaMalloc(&d_output, n_output));
+// 			//Allocate memories
+// 			float* d_data, * d_output;
+// 			size_t n_data = height_out * width_out * height_kernel * width_kernel * channel_in * sizeof(float);
+// 			size_t n_output = height_out * width_out * channel_out * sizeof(float);
+// 			CHECK(cudaMalloc(&d_data, n_data));
+// 			CHECK(cudaMalloc(&d_output, n_output));
 			
-			//Grid size and Block size
-			dim3 blockSize (32, 32); //default
-			dim3 gridSize((channel_out - 1) / blockSize.x + 1,
-				      (height_out * width_out - 1) / blockSize.y + 1);
+// 			//Grid size and Block size
+// 			dim3 blockSize (32, 32); //default
+// 			dim3 gridSize((channel_out - 1) / blockSize.x + 1,
+// 				      (height_out * width_out - 1) / blockSize.y + 1);
 			
-			im2col<<<gridSize, blockSize>>>(d_input, d_data, height_in, width_in, height_kernel, width_kernel, stride);
-			convolution<<<gridSize, blockSize>>>(d_data, d_weight, d_output, d_bias, height_out * width_out, 
-							     height_kernel * width_kernel * channel_in, channel_out);
+// 			im2col<<<gridSize, blockSize>>>(d_input, d_data, height_in, width_in, height_kernel, width_kernel, stride);
+// 			convolution<<<gridSize, blockSize>>>(d_data, d_weight, d_output, d_bias, height_out * width_out, 
+// 							     height_kernel * width_kernel * channel_in, channel_out);
 			
-			//TODO: Copy data from d_output to out
-			Vector out;
-			out.resize(height_out * width_out * channel_out);
+// 			//TODO: Copy data from d_output to out
+// 			Vector out;
+// 			out.resize(height_out * width_out * channel_out);
 			
-			//TODO: Add out to top.col(n)
-		}
-	}
-}
+// 			//TODO: Add out to top.col(n)
+// 		}
+// 	}
+// }
