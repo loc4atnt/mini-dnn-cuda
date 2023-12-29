ce7d6c2db14a1b881a8c5eecd3eff46e80e651c5
NhatPhuong2106 <116291724+NhatPhuong2106@users.noreply.github.com>
2023-12-29 14:18:52 +0700

    Update Operator.cu

--- src/device/Operator.cu
+++ src/device/Operator.cu
@@ -279,7 +279,11 @@ void dev_convForward(float *out, float *in, float *wei, float *bias,
           (hw_out - 1) / blockSize.y + 1);
 
   im2col<<<gridSize, blockSize>>>(d_input, d_data, h_in, w_in, ch_in, h_ker, w_ker, h_out, w_out, ch_out, stride);
+  CHECK(cudaDeviceSynchronize());
+  CHECK(cudaGetLastError());
   convolution<<<gridSize, blockSize>>>(d_data, d_weight, d_output, d_bias, hw_out, hw_ker * ch_in, ch_out);
+  CHECK(cudaDeviceSynchronize());
+  CHECK(cudaGetLastError());
 
   //TODO: Copy data from d_output to out
   CHECK(cudaMemcpy(out, d_output, n_output, cudaMemcpyDeviceToHost));
